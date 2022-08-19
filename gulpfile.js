4fb4c3abdf558b5aaa7b70dde0e4858f5d511c43
Vitalii-Fed <aevetal@gmail.com>
2022-08-19 21:48:10 +0300

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,15 @@ const sass = require('gulp-sass')(require('sass'));
 const csso = require('gulp-csso');
 const rename = require('gulp-rename');
 const del = require('del');
+const autoprefixer = require('autoprefixewr');
+const postcss = require('gulp-postcss');
 
 gulp.task('scss', () => {
     return gulp.src('./scss/**/*.scss')
     .pipe(sass())
+    .pipe(postcss([
+        autoprefixer()
+    ]))
     .pipe(csso())
     .pipe(rename({
         suffix: '.min',
