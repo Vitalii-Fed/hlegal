cda054fed9f47777ae476bb56741040bb83198e7
Vitalii-Fed <aevetal@gmail.com>
2022-08-19 19:12:08 +0300

    inst minify and del

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const csso = require('gulp-csso');
+const rename = require('gulp-rename');
+const del = require('del');
 
 gulp.task('scss', () => {
     return gulp.src('./scss/**/*.scss')
     .pipe(sass())
+    .pipe(csso())
+    .pipe(rename({
+        suffix: '.min',
+    }))
     .pipe(gulp.dest('./css'));
 });
 
+gulp.task("clean", () => {
+    return del('css')
+})
+
 gulp.watch('./scss/**/*.scss', gulp.series('scss'));
-gulp.task('start', gulp.series('scss'));
+gulp.task('start', gulp.series('clean', 'scss'));
